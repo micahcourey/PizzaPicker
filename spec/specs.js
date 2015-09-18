1324b0c41562b8dda42f13ae403ea4f427f7d1e2
micahcourey <micahjcourey@gmail.com>
2015-09-18 11:44:32 -0700

    create tests for all pizza sizes and pass

--- spec/specs.js
+++ spec/specs.js
@@ -8,8 +8,20 @@ describe('Pizza', function() {
 });
 
 describe('pizzaSizePrice', function() {
-  it("is $6 if its a small pizza", function() {
+  it("is $8 if its a small pizza", function() {
+    var testPizza = new Pizza("Small", "Cheese", 1);
+    expect(testPizza.pizzaSizePrice()).to.equal(8);
+  });
+  it("is $10 if its a medium pizza", function() {
+    var testPizza = new Pizza("Medium", "Cheese", 1);
+    expect(testPizza.pizzaSizePrice()).to.equal(10);
+  });
+  it("is $12 if its a large pizza", function() {
     var testPizza = new Pizza("Large", "Cheese", 1);
     expect(testPizza.pizzaSizePrice()).to.equal(12);
+  });
+  it("is $14 if its a X-Large pizza", function() {
+    var testPizza = new Pizza("X-Large", "Cheese", 1);
+    expect(testPizza.pizzaSizePrice()).to.equal(14);
   });
 });
